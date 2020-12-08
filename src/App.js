1a5add575f2c60c8068916f24d86c4b13bb07f08
Anton Webb <anton25360@gmail.com>
2020-12-08 13:30:44 +0000

    Added image to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import image from "./assets/img/launch-home.png";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="appContainer">
+        <img className="appImage" src={image} />
+        <div className='appTest'></div>
+      </div>
     </div>
   );
 }
