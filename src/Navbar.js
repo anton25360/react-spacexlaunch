3d730ab8ec546863474744e3979ff3f851b5bf83
Anton Webb <anton25360@gmail.com>
2020-12-08 11:50:56 +0000

    styled logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,10 @@ import logo from "./assets/spacex-logo.png";
 function Navbar() {
   return (
     <div className="navbarContainer">
-      <img src={logo} />
+      <div className="navbarLogoContainer">
+        <img className="navbarLogoImage" src={logo} />
+        <p className="navbarLogoText">LAUNCHES</p>
+      </div>
     </div>
   );
 }
