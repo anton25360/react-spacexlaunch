ca5fb9e2fc4d0ecfdb737264acae74b94664db2d
Anton Webb <anton25360@gmail.com>
2020-12-08 12:24:33 +0000

    styled reload button

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import "./Navbar.css";
 import logo from "./assets/spacex-logo.png";
+import reload from "./assets/icon/refresh.png";
 
 function Navbar() {
   return (
@@ -8,7 +9,9 @@ function Navbar() {
         <img className="navbarLogoImage" src={logo} />
         <p className="navbarLogoText">LAUNCHES</p>
       </div>
-      <div className="navbarReload">Reload Data</div>
+      <div className="navbarReload">
+        Reload Data <img className="navbarReloadIcon" src={reload} />
+      </div>
     </div>
   );
 }
