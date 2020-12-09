59a4efeaa54b079b1d32f4f278911aa567dc5fd9
Anton Webb <anton25360@gmail.com>
2020-12-09 16:09:51 +0000

    added icons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 import image from "./assets/img/launch-home.png";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar";import sort from "./assets/icon/sort.png";
+import select from "./assets/icon/select.png";
 
-let isAscending =  true
+let isAscending = true;
 
 function switchSort() {
-  let btn = document.getElementById('btnSort')
+  let btn = document.getElementById("btnSort");
 
   if (isAscending == true) {
-    btn.innerText = 'Sort Descending'
-    isAscending =false
+    btn.innerText = "Sort Descending";
+    isAscending = false;
   } else if (isAscending == false) {
-    btn.innerText = 'Sort Ascending'
-    isAscending =true
+    btn.innerText = "Sort Ascending";
+    isAscending = true;
   }
-
 }
 
 function App() {
@@ -25,8 +25,16 @@ function App() {
         <img className="appImage" src={image} alt="rocket launch" />
         <div className="appTest">
           <div className="appButtonsContainer">
-            <button className="appButton">Filter by Year</button>
-            <button className="appButton" onClick={()=>switchSort()} id='btnSort'>Sort Ascending</button>
+            <button className="appButton">
+              Filter by Year <img src={select} />
+            </button>
+            <button
+              className="appButton"
+              onClick={() => switchSort()}
+              id="btnSort"
+            >
+              Sort Ascending <img className="appButtonSortIcon" src={sort} />
+            </button>
           </div>
           <div className="appContent">
             <p>hellotherebro</p>
