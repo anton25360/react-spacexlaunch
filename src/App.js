f990b06a6c461b9a5a196284325d861ccfe75990
Anton Webb <anton25360@gmail.com>
2020-12-09 15:49:20 +0000

    Added filter buttons

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="appContainer">
-        <img className="appImage" src={image} alt='rocket launch' />
-        <div className='appTest'></div>
+        <img className="appImage" src={image} alt="rocket launch" />
+        <div className="appTest">
+          <div className="appButtonsContainer">
+            <button className="appButtonFilterByYear">Filter by Year ICON</button>
+            <button className="appButtonSortAscending">Sort Ascending ICON</button>
+          </div>
+          <div className="appContent">
+            <p>hellotherebro</p>
+            <p>hellotherebro</p>
+            <p>hellotherebro</p>
+            <p>hellotherebro</p>
+            <p>hellotherebro</p>
+          </div>
+        </div>
       </div>
     </div>
   );
