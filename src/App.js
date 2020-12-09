db8fbc75553c22036547d8aaf682bcd6e9a15a41
Anton Webb <anton25360@gmail.com>
2020-12-09 16:05:54 +0000

    ascending boolean flips on btn press

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import "./App.css";
 import image from "./assets/img/launch-home.png";
 import Navbar from "./Navbar";
 
+let isAscending =  true
+
+function switchSort() {
+  let btn = document.getElementById('btnSort')
+
+  if (isAscending == true) {
+    btn.innerText = 'Sort Descending'
+    isAscending =false
+  } else if (isAscending == false) {
+    btn.innerText = 'Sort Ascending'
+    isAscending =true
+  }
+
+}
+
 function App() {
   return (
     <div className="App">
@@ -11,7 +26,7 @@ function App() {
         <div className="appTest">
           <div className="appButtonsContainer">
             <button className="appButton">Filter by Year</button>
-            <button className="appButton">Sort Ascending</button>
+            <button className="appButton" onClick={()=>switchSort()} id='btnSort'>Sort Ascending</button>
           </div>
           <div className="appContent">
             <p>hellotherebro</p>
