361de4812c42ebd9b983f0ee662c3325171fc81f
Anton Webb <anton25360@gmail.com>
2020-12-10 15:16:45 +0000

    Styled item component

--- src/Item.js
+++ src/Item.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./Item.css";
 
-
 export default class Item extends Component {
-    render() {
-        return (
-            <div className='itemContainer'>
-            <p className='itemNumber'>{this.props.number}</p>
-            <p className='itemName'>{this.props.name}</p>
-            <p className='itemDate'>{this.props.date}</p>
-            <p className='itemRocket'>{this.props.rocket}</p>
+  render() {
+    return (
+      <div className="itemContainer">
+        <p className="itemNumber itemElement">#{this.props.number}</p>
+        <p className="itemName itemElement">{this.props.name}</p>
+
+        <div className="itemRight">
+          <p className="itemDate itemElement">{this.props.date}</p>
+          <p className="itemRocket itemElement">{this.props.rocket}</p>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
